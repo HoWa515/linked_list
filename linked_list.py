36f36571f4feb5a103586c3eb4819f018bbbcf2a
wh <sunfortea123@gmail.com>
2025-04-17 07:21:14 +0200

    m

--- linked_list.py
+++ linked_list.py
@@ -1,9 +1,10 @@
-# node
+# -------node
 class Node:
     def __init__(self,value):
         self.value=value
         self.next = None
 
+# ------linked list
 class Linked_List:
     def __init__(self,value):
         new_node = Node(value)
@@ -18,7 +19,19 @@ class Linked_List:
             temp=temp.next
 
 
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            self.tail=new_node
+        self.length+=1
+        return True  # pass to function calls it
 
-my_list=Linked_List(5)
-print(my_list.head.value)
-print(my_list.tail.next)
+l=Linked_List(1)
+l.append(2)
+l.print_list()
+
+
