ff3f692dc69f5c4e5ef0d9e44353ef06c663ed6a
wh <sunfortea123@gmail.com>
2025-04-17 07:09:35 +0200

    m

--- linked_list.py
+++ linked_list.py
@@ -11,6 +11,14 @@ class Linked_List:
         self.tail=new_node
         self.length=1
 
+    def print_list(self):
+        temp=self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
+
+
+
 my_list=Linked_List(5)
 print(my_list.head.value)
 print(my_list.tail.next)
